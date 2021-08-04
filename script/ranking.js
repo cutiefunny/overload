80a75f23194a1e9e396cff5635dff88e266e53b0
cutiefunny <cutiefunny@gmail.com>
2021-08-04 11:18:31 +0900

    menWomen

--- script/ranking.js
+++ script/ranking.js
@@ -1,3 +1,22 @@
+//#region 초반 선언부
+var btn_men = document.getElementById('btn_men');
+var btn_women = document.getElementById('btn_women');
+var div_men = document.getElementById('div_men');
+var div_women = document.getElementById('div_women');
+//#endregion
+
+//men 버튼 클릭
+function selectMen(){
+    div_men.setAttribute("style","display:block"); 
+    div_women.setAttribute("style","display:none"); 
+}
+//women 버튼 클릭
+function selectWomen(){
+    div_men.setAttribute("style","display:none"); 
+    div_women.setAttribute("style","display:block"); 
+}
+
+//유저 삭제 : 관리자 기능
 function delUser(userID){
     if(confirm(userID+" 삭제하시겠습니까?")){
         callAjax("delUser",userID);
