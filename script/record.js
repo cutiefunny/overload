513900a3d0989574c0a624c34c535def01d5c85f
준환 김 <cutiefunny@naver.com>
2021-08-06 00:18:38 +0900

    img

---
+++ script/record.js
@@ -0,0 +1,7 @@
+var div_squat = document.getElementById('div_squat');
+
+//페이지 시작 시 수행되는 함수
+window.onload = function(){
+    //div_squat.setAttribute("style","display:inline");
+    div_squat.textContent = "te";
+};
